9627860427962d1b7cd63a44db4a9654c255c95c
Diego <diegomez27@outlook.com>
2026-02-13 12:01:06 -0700

    feat: remove specific route configuration for Pokémon prerendering

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,15 +2,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'pokemon/:id',
-    renderMode: RenderMode.Prerender,
-    async getPrerenderParams() {
-      // Prerenderizar los primeros 151 pokémon (primera generación)
-      const ids = Array.from({ length: 151 }, (_, i) => ({ id: `${i + 1}` }));
-      return ids;
-    }
-  },
-  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
