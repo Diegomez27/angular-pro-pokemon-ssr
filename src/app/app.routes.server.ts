bf36c4e7073a0d9dbe28e46afb9ecd00270b3aa0
Diego <diegomez27@outlook.com>
2026-02-13 11:07:01 -0700

    feat: add prerendering for the first 151 Pokémon

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'pokemon/:id',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      // Prerenderizar los primeros 151 pokémon (primera generación)
+      const ids = Array.from({ length: 151 }, (_, i) => ({ id: `${i + 1}` }));
+      return ids;
+    }
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
